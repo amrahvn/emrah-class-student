c2347a3ba7a5b9070e220b8c6f28528b9489d69f
Amrahvn <amrahvn@code.edu.az>
2023-04-09 01:45:36 +0400

    Add project files.

---
+++ emrah class-student/Program.cs
@@ -0,0 +1,24 @@
+﻿Student student1 = new Student("Emrax", "Nesirov", "P-234", 100, "mezun");
+Student student2 = new Student("Adil", "Hesenov", "p-234", 120, "mezun deyil");
+Student student3 = new Student("Elmar", "Haciyev", "p-234", 60, "mezun deyil");
+
+
+student1.GetFullInfo();
+
+Console.WriteLine("   ");
+
+student2.GetFullInfo();
+
+Console.WriteLine("    ");
+
+student3.GetFullInfo();
+
+Console.WriteLine( "  ");
+
+student1.GetFullName();
+
+
+
+
+
+
---
+++ emrah class-student/Student.cs
@@ -0,0 +1,52 @@
+﻿
+internal class Student
+{
+    public string Name;
+    public string Surname;
+    public string Group;
+    public double Point;
+    public string isGraduated;
+
+    public Student(string name, string surname, string group, double point, string isGraduated)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Group = group;
+        this.Point = point;
+        this.isGraduated = isGraduated;
+    }
+
+    public void GetFullName()
+    {
+        Console.WriteLine(Name + " " + Surname);
+
+    }
+
+    public void GetFullInfo()
+    {
+        Console.WriteLine($"{"Name:" + this.Name},{"Surname:" + this.Surname},{"Group:" + this.Group},{"Point:" + this.Point},{this.isGraduated}");
+        if (isGraduated == "mezun")
+        {
+            Console.WriteLine("Congratulations, you graduated");
+        }
+        else if (isGraduated == "mezun deyil")
+        {
+            Console.WriteLine("I'm sorry you didn't graduate");
+        }
+        if (Point >= 80&&Point<=100)
+        {
+            Console.WriteLine("you can enter the second exam");
+        }
+        else if (Point <= 80 && Point >= 0)
+        {
+            Console.WriteLine("you cannot enter the second exam");
+        }
+
+         if (Point < 0 || Point > 100)
+        {
+            Console.WriteLine("point is not correct");
+            
+        }
+    }
+}
+
